a7b8f9e01cb6ab57dd9e7ceea45eb9af79f1748d
Sreeja Gupta <sregupta@thoughtworks.com>
2019-06-23 16:47:44 +1000

    Added neopixel helper to step 2

--- step2/neopixel_helper.py
+++ step2/neopixel_helper.py
@@ -1,5 +1,14 @@
 import neopixel
 import board
+import time
+
+DARK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 150, 0)
+GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
+BLUE = (0, 0, 255)
+PURPLE = (180, 0, 255)
 
 # Initialize the output pin on the Gemma M0 that is connected to the NeoPixel
 pixel_pin = board.D0
@@ -10,16 +19,32 @@ num_pixels = 1
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.1, auto_write=True)
 
 def off():
-    pixels.fill((0,0,0))
+    pixels.fill(DARK)
 
 def green():
-    pixels.fill((0, 255, 0))
+    pixels.fill(GREEN)
 
 def red():
-    pixels.fill((255, 0, 0))
+    pixels.fill(RED);
 
 def blue():
-    pixels.fill((0, 0, 255))
+    pixels.fill(BLUE);
+
+def rainbow():
+    for color in [PURPLE, BLUE, CYAN, GREEN, YELLOW, RED]:
+        pixels.fill(color)
+        time.sleep(0.5)
 
 def set_brightness(brightness):
     pixels.brightness = brightness;
+
+def _wheel(pos):
+    if pos < 0 or pos > 255:
+        return (0, 0, 0)
+    if pos < 85:
+        return (255 - pos * 3, pos * 3, 0)
+    if pos < 170:
+        pos -= 85
+        return (0, 255 - pos * 3, pos * 3)
+    pos -= 170
+    return (pos * 3, 0, 255 - pos * 3)
