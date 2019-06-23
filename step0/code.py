e8a9ab9d54dd35e7fdb5b2ebadb4f28937492246
Sreeja Gupta <sregupta@thoughtworks.com>
2019-06-23 15:51:52 +1000

    Added more comments in step 0

--- step0/code.py
+++ step0/code.py
@@ -5,8 +5,9 @@ import time
 led = digitalio.DigitalInOut(board.D13)
 led.direction = digitalio.Direction.OUTPUT
 
+# Step 0 blink a red LED on and off
 while True:
-    #Turn LED on by setting led.value to True
+    #Turn LED on by setting led.value to True below
     time.sleep(0.5)
-    #Turn LED on by setting led.value to False
+    #Turn LED on by setting led.value to False below
     time.sleep(0.5)
