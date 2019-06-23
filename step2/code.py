23a2e47a573e7294971815b5ca7f0dee681b32d1
Sreeja Gupta <sregupta@thoughtworks.com>
2019-06-23 16:55:03 +1000

    Added step 2 code

--- step2/code.py
+++ step2/code.py
@@ -1,23 +1,42 @@
+"""
+Step 2:
+
+Hook up your sensor input to your NeoPixel light output!
+
+"""
+
 import board
 import analogio
 import time
 import neopixel_helper
 
 photocell = analogio.AnalogIn(board.A1)
-VOLTAGE_IN_NORMAL_LIGHT = 2.85;
+# Pop in your voltage value from Step 1 below
+VOLTAGE_IN_NORMAL_LIGHT = 9999
 
 def analog_voltage(adc):
     return adc.value / 65535 * adc.reference_voltage
 
 def calculate_brightness(volts):
-    return round((1 - (1/VOLTAGE_IN_NORMAL_LIGHT) * volts), 1);
+    return round((1 - (1/VOLTAGE_IN_NORMAL_LIGHT) * volts), 1)
 
 while True:
-    neopixel_helper.off();
-    volts = analog_voltage(photocell)
-    neopixel_helper.green();
-    print('Photocell voltage: {0}V'.format(volts));
-    brightness = calculate_brightness(volts);
-    print("Brightness: ", brightness);
-    neopixel_helper.set_brightness(brightness);
-    time.sleep(1.0);
+    # Initially set light to off
+    neopixel_helper.off()
+    # Read voltage
+    current_voltage = analog_voltage(photocell)
+    # Calculate brightness on a scale on 0-1 (0.5 is medium brightness)
+    brightness = calculate_brightness(current_voltage);
+    print('Brightness: {}'.format(brightness));
+
+    """
+    Now get creative! Use if statements to wire up brightness values to neopixel functions.
+
+    Eg: if (current_voltage > VOLTAGE_IN_NORMAL_LIGHT):
+            neopixel_helper.rainbow()
+
+            OR
+
+        neopixel_helper.set_brightness(brightness)
+
+    """
