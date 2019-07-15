2cfd49b7ce05a408be089b68e9bc4b9d72ea700c
Sreeja <sreejagpt@gmail.com>
2019-07-15 15:00:55 -0700

    Update code.py

--- step1/code.py
+++ step1/code.py
@@ -12,9 +12,6 @@ we can tell when it gets brighter or darker relative to the room
 
 photocell = analogio.AnalogIn(board.A1);
 
-# Find out what is getting printed out in your serial console and set VOLTAGE_IN_NORMAL_LIGHT to that value below:
-VOLTAGE_IN_NORMAL_LIGHT = 9999999;
-
 """
 A function that calculates the voltage measured across the light sensor
 """
@@ -25,4 +22,3 @@ while True:
     volts = analog_voltage(photocell);
     print('Voltage in normal light: {0}\n\n'.format(volts));
     time.sleep(1.0);
-
