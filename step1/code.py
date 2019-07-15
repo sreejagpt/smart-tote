82dfe130c62a40defb948e7e710ab232fb59f2a4
Sreeja <sreejagpt@gmail.com>
2019-07-15 14:43:30 -0700

    Update code.py

--- step1/code.py
+++ step1/code.py
@@ -5,16 +5,19 @@ import time
 """
 Step 1
 
-Calibrating our light sensor to the brightness of the room we are in
+Calibrating our light sensor to the brightness of the room we are in so that
+we can tell when it gets brighter or darker relative to the room
 
 """
-
 
 photocell = analogio.AnalogIn(board.A1);
 
 # Find out what is getting printed out in your serial console and set VOLTAGE_IN_NORMAL_LIGHT to that value below:
-VOLTAGE_IN_NORMAL_LIGHT = 999999;
+VOLTAGE_IN_NORMAL_LIGHT = 9999999;
 
+"""
+A function that calculates the voltage measured across the light sensor
+"""
 def analog_voltage(adc):
     return adc.value / 65535 * adc.reference_voltage;
 
@@ -22,3 +25,4 @@ while True:
     volts = analog_voltage(photocell);
     print('Voltage in normal light: {0}\n\n'.format(volts));
     time.sleep(1.0);
+
