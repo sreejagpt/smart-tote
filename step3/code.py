1070d65f8cb3a0cee31040b8f894be0c9f45d1a4
Sreeja <sreejagpt@gmail.com>
2019-07-17 11:09:56 -0700

    Update code.py

--- step3/code.py
+++ step3/code.py
@@ -32,33 +32,10 @@ def calculate_brightness(volts):
 while True:
     neopixel_helper.off();
     volts = analog_voltage(photocell)
-    brightness = calculate_brightness(volts);
+    led_brightness = calculate_brightness(volts);
     
     # Use what you learned in Step2 to program your LEDs based on brightness!
     # You can change colours and brightness based on how bright or dim it is outside.
     # Try it below!
 
-    time.sleep(1.0);
-import board
-import analogio
-import time
-import neopixel_helper
-
-photocell = analogio.AnalogIn(board.A1)
-VOLTAGE_IN_NORMAL_LIGHT = 2.85;
-
-def analog_voltage(adc):
-    return adc.value / 65535 * adc.reference_voltage
-
-def calculate_brightness(volts):
-    return round((1 - (1/VOLTAGE_IN_NORMAL_LIGHT) * volts), 1);
-
-while True:
-    neopixel_helper.off();
-    volts = analog_voltage(photocell)
-    neopixel_helper.green();
-    print('Photocell voltage: {0}V'.format(volts));
-    brightness = calculate_brightness(volts);
-    print("Brightness: ", brightness);
-    neopixel_helper.set_brightness(brightness);
     time.sleep(1.0);
